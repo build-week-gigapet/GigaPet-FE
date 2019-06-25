59cdb70e26231a4a7cf7e57649af9fe9c57264f0
johnschneider1 <johnlawschneider@gmail.com>
2019-06-25 17:12:45 -0500

    updated carousel

--- JS/landCarousel.js
+++ JS/landCarousel.js
@@ -1,22 +1,21 @@
 class Carousel {
-    constructor(element) {
-      this.element = element;
-      this.prev = this.element.querySelector(".left-button");
-      this.next = this.element.querySelector(".right-button");
-      this.pics = this.element.querySelectorAll("img");
-      this.index = 0;
-      this.prev.addEventListener("click", () => this.picShift(-1));
-      this.next.addEventListener("click", () => this.picShift(1));
-      this.pics[this.index].style.display = "block";
-    }
-    picShift(dir) {
-      this.index += dir;
-      this.pics[this.index].style.display = "block";
-      this.pics[this.index - dir].style.display = "none";
-    }
-
-
-
+  constructor(element) {
+    this.element = element;
+    this.prev = this.element.querySelector(".left-button");
+    this.next = this.element.querySelector(".right-button");
+    this.pics = this.element.querySelectorAll("img");
+    this.index = 0;
+    this.prev.addEventListener("click", () => this.picShift(-1));
+    this.next.addEventListener("click", () => this.picShift(1));
+    this.pics[this.index].style.display = "block";
+    console.log(this.pics);
+  }
+  picShift(dir) {
+    this.index += dir;
+    this.pics[this.index].style.display = "block";
+    this.pics[this.index - dir].style.display = "none";
+  }
+}
 
 let carousel = document.querySelector(".carousel");
 carousel = new Carousel(carousel);
