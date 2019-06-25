8c85ffda9141772a7d93b32088fc0425423248cd
danRolon <daniel.rolon@yahoo.com>
2019-06-24 23:42:52 -0500

    Add better structure to carousel in about.html -start styling the carousel in carousel.less

---
+++ JS/carousel.js
@@ -0,0 +1,18 @@
+class Carousel{
+  constructor(element){
+    this.element = element;
+    this.tabs = element.querySelectorAll('.tab');
+    this.tmpNode = tabs[0];
+
+    this.leftBtn = element.querySelector('.btn-left');
+    this.rightBtn = element.querySelector('.btn-right');
+  }
+
+  moveLeft(){
+
+  }
+
+  moveRight(){
+
+  }
+}
