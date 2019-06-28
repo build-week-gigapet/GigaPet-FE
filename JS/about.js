9092f0ce4b6e39e494bc7dafe4694639f9289616
danRolon <daniel.rolon@yahoo.com>
2019-06-28 01:45:59 -0500

    fixing formatting on documents

--- JS/about.js
+++ JS/about.js
@@ -1,8 +1,8 @@
 const letters = document.querySelectorAll('.intro h1 span');
 
-letters.forEach(letter=>letter.addEventListener('mouseover', ()=>{
+letters.forEach(letter => letter.addEventListener('mouseover', () => {
   letter.classList.add('bounceDown');
-  letter.addEventListener('animationend',()=>{
+  letter.addEventListener('animationend', () => {
     letter.classList.remove('bounceDown');
   });
 }));
--- JS/aboutCarousel.js
+++ JS/aboutCarousel.js
@@ -1,5 +1,5 @@
-class Carousel{
-  constructor(element){
+class Carousel {
+  constructor(element) {
     this.element = element;
     this.tabs = element.querySelectorAll('.tab');
     this.tabData = this.tabs[2].dataset.tab;
@@ -8,20 +8,20 @@ class Carousel{
     this.leftBtn = element.querySelector('.btn-left');
     this.rightBtn = element.querySelector('.btn-right');
 
-    this.leftBtn.addEventListener('click', ()=>this.moveLeft());
-        this.rightBtn.addEventListener('click', ()=>this.moveRight());
+    this.leftBtn.addEventListener('click', () => this.moveLeft());
+    this.rightBtn.addEventListener('click', () => this.moveRight());
   }
 
-  moveLeft(){
-    const tmpNode =this.tabs[0];
+  moveLeft() {
+    const tmpNode = this.tabs[0];
     this.tabs[0].className = 'tab move-out-from-left';
     document.querySelector('.card-active').classList.remove('card-active');
     document.querySelector('.cards').classList.add('shrink');
-    
-    setTimeout(()=>{
-      if(this.tabs.length>5){
+
+    setTimeout(() => {
+      if (this.tabs.length > 5) {
         tmpNode.classList.add('tab-hide');
-        this.tabs[5].className ='tab move-to-position5-from-left';
+        this.tabs[5].className = 'tab move-to-position5-from-left';
       }
       this.tabs[1].className = "tab move-to-position1-from-left";
       this.tabs[2].className = "tab move-to-position2-from-left";
@@ -30,26 +30,26 @@ class Carousel{
       this.tabs[0].remove();
       this.element.querySelector('.tab-container').appendChild(tmpNode);
       this.tabs = this.element.querySelectorAll('.tab');
-      
+
       this.tabData = this.tabs[2].dataset.tab;
       this.card = document.querySelector(`.card[data-tab= '${this.tabData}']`);
       this.card = new TabCard(this.card);
-      document.querySelector('.cards').classList.remove('shrink'); 
+      document.querySelector('.cards').classList.remove('shrink');
       this.card.selectCard();
-      
+
 
     }, 500)
 
   }
 
-  moveRight(){
-    this.tabs[4].className ='tab move-out-from-right';
+  moveRight() {
+    this.tabs[4].className = 'tab move-out-from-right';
     document.querySelector('.card-active').classList.remove('card-active');
     document.querySelector('.cards').classList.add('shrink');
-    
-    setTimeout(()=>{
+
+    setTimeout(() => {
       const tabNum = this.tabs.length;
-      if(tabNum>4){
+      if (tabNum > 4) {
         this.tabs[4].className = 'tab tab-hide'
       }
       const tmpNode = this.tabs[this.tabs.length - 1];
@@ -75,18 +75,18 @@ class Carousel{
   }
 }
 
-class TabCard{
-  constructor(element){
+class TabCard {
+  constructor(element) {
     this.element = element;
   }
-  selectCard(){
+  selectCard() {
     this.element.classList.add('card-active');
-    setTimeout(function(){
+    setTimeout(function () {
       //window.scrollTo(0,document.querySelector('.cards').offsetTop + 600);
-      document.querySelector('.cards').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-    },500);
+      document.querySelector('.cards').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }, 500);
   }
 }
 
 //let carousel = new Carousel(document.querySelector('.carousel'));
-let carousel = document.querySelectorAll('.carousel').forEach(caro=>new Carousel(caro));
+let carousel = document.querySelectorAll('.carousel').forEach(caro => new Carousel(caro));
